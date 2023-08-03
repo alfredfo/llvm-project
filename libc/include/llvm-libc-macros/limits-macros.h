0be53784f0b35e98eb52b0630492c940c32720e1
Alfred Persson Forsberg <cat@catcream.org>
2023-08-03 04:36:01 +0200

    [libc] Add limits.h
    Signed-off-by: Alfred Persson Forsberg <cat@catcream.org>

---
+++ libc/include/llvm-libc-macros/limits-macros.h
@@ -0,0 +1,8 @@
+#ifndef __LLVM_LIBC_MACROS_LIMITS_MACROS_H
+#define __LLVM_LIBC_MACROS_LIMITS_MACROS_H
+
+#ifdef __linux__
+#include <linux/limits.h>
+#endif
+
+#endif // __LLVM_LIBC_MACROS_LIMITS_MACROS_H
